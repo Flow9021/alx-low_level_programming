197fbaeb33eced175ffbe20bb34fd9d94f2ddcc4
Flow9021 <floransiah@gmail.com>
2023-09-08 08:01:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main- entry point for the
+ * function.
+ * it invokes printf to print out a given string
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
