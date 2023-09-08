fec29aa722a7c86ed4940c3e25da9db0ad460908
Flow9021 <floransiah@gmail.com>
2023-09-08 07:11:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+/**
+ * puts-prints a given string
+ * followed by a new line
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzel");
+	return (0);
+
+}
