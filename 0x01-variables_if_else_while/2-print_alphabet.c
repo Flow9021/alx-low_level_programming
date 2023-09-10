a30e8b86698217acaada595e10355f32e6f7bb49
Flow9021 <floransiah@gmail.com>
2023-09-10 13:28:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
