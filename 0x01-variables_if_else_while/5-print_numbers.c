4ce1de4d884619144e8b328d5977230771630448
Flow9021 <floransiah@gmail.com>
2023-09-10 14:52:24 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d\n", i);
+	return (0);
+}
