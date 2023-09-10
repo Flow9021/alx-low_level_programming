d87db8d6e7841ce43fd4fe8cc7cd566681813cbe
Flow9021 <floransiah@gmail.com>
2023-09-10 15:53:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
