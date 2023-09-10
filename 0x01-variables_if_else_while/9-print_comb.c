15d220b2ec59e4e7159cc6e55936ef09c7af9a14
Flow9021 <floransiah@gmail.com>
2023-09-10 16:46:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		if (i < 9)
-			putchar(i + '0');
-	putchar(',');
-	putchar(' ');
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(' , ');
+			putchar('   ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
