b73e9e33701ff5130d1a483d24b43a75d2d6cf5a
Flow9021 <floransiah@gmail.com>
2023-09-10 16:00:24 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,12 @@
 int main(void)
 {
 	int i;
+	char x;
 
-	for (i = '0'; i <= 'f'; i++)
+	for (i = '0'; i <= '9'; i++)
 		putchar(i);
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
 	putchar('\n');
 	return (0);
 }
