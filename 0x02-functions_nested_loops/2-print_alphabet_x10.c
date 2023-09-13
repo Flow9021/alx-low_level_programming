9ea3c805fd8b81d6d181a3d1f2da9f3abd78b5c8
Flow9021 <floransiah@gmail.com>
2023-09-13 14:31:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase 10 times
+ *
+ * Return: 0 as success
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+		_putchar('\n');
+	}
+}
