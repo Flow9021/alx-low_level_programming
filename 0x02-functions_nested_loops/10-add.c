59270ef3f36bd44aece1b6d9b692384c56e30fcb
Flow9021 <floransiah@gmail.com>
2023-09-15 14:02:20 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two interger
+ * @a: parameter of function
+ * @b: parameter of function
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
