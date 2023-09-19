a98e46ded3bd54bc663a7e6be36aacb4097f20a8
Flow9021 <floransiah@gmail.com>
2023-09-19 14:01:14 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of a variable
+ *@*n: deferenced value of pointer n
+ *Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
