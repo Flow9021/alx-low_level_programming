6904f96a9b013e2cab433af5a3a4422bb0d11bf5
Flow9021 <floransiah@gmail.com>
2023-09-19 18:28:50 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * dest: parameter of the funtion
+ * src: parameter of the function
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
