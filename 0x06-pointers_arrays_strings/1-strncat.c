4c2ecac4bc003159de9a0f3ac4e086cb8b8f8eea
Flow9021 <floransiah@gmail.com>
2023-09-22 15:38:09 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: parameter of the function
+ * @src: parameter of the function
+ * @n: parameter of the function
+ * Return: p
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+		while (*dest != '\0')
+		{
+			dest++;
+		}
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (p);
+}
