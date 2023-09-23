7be93cec9c0bc661f69a6837c4908e9b574e9ac5
Flow9021 <floransiah@gmail.com>
2023-09-23 15:40:19 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,34 +4,22 @@
  * _strcmp - compares two strings
  * @s1: parameter of the function
  * @s2: parameter of the function
- * Return: 1 or 0 or -1
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (*s1 < *s2)
+		if (s1[i] != s2[i])
 		{
-			return (-1);
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s2[i] < s1[i])
+				return (s1[i] - s2[i]);
 		}
-		else if (*s1 > *s2)
-		{
-			return (1);
-		}
-		s1++;
-		s2++;
 	}
-	if (*s1 == '\0' && *s2 != '\0')
-	{
-		return (-1);
-	}
-	else if (*s1 != '\0' && *s2 == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string -  capitalizes all words of a string
+ * @s: analized string
+ * Return: string with all words capitalized
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, a;
+
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	int = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a(j) == *(a + i - 1))
+					{
+						*(s + i) = *(a + i) - 32;
+					}
+				}
+			}
+		}
+	i++
+	}
+	return (s);
