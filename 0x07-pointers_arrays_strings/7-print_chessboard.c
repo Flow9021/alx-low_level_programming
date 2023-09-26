01e955219d19d0b4834cec0f69ea5194dfcfb56d
Flow9021 <floransiah@gmail.com>
2023-09-26 13:32:08 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: parameter of the function
+ * @8: parameter of the function
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int l;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (l = 0; l < 8; l++)
+		{
+			printf("%c ", a[k][l]);
+		}
+		printf("\n");
+	}
+}
