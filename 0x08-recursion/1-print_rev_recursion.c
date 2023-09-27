1da7d6b84dd776ee96eb740acc3957a5c3445b7b
Flow9021 <floransiah@gmail.com>
2023-09-27 16:31:12 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,12 +10,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i;
-	int len = strlen(s);
-
-	for (i = len - 1; i > 0; i--)
+	if (*s == '\0')
 	{
-		putchar(s[i]);
+		return;
 	}
-	putchar('\n');
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
