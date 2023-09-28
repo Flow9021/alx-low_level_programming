86d2d36d605caf9756253add12f470873f1e87c2
Flow9021 <floransiah@gmail.com>
2023-09-27 19:26:20 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: parameter of the function
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int start = 1;
+		int end = n;
+
+		while (start <= end)
+		{
+			int mid = (start + end) / 2;
+
+			if (mid * mid == n)
+			{
+				return (mid);
+			}
+			else if (mid * mid < n)
+			{
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		return (-1);
+	}
+}
