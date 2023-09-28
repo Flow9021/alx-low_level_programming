2af87215316dc809ca033dc7c3b6c66834af85d2
Flow9021 <floransiah@gmail.com>
2023-09-27 17:07:34 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		length++;
-		s++;
+		return (0);
 	}
-	return (length);
+	else
+	{
+		return (1 + (int)_strlen_recursion(s + 1));
+	}
 }
