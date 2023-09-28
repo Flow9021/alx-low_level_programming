67c4330bfb3bf058b7727a6df8a69f5555f0b188
Flow9021 <floransiah@gmail.com>
2023-09-27 20:17:40 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,36 +8,23 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		int start = 1;
-		int end = n;
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
 
-		while (start <= end)
-		{
-			int mid = (start + end) / 2;
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ * Return: 1 if n is prime, 0 if not
+ */
 
-			if (mid * mid == n)
-			{
-				return (mid);
-			}
-			else if (mid * mid < n)
-			{
-				start = mid + 1;
-			}
-			else
-			{
-				end = mid - 1;
-			}
-		}
-		return (-1);
-	}
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
