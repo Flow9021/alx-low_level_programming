a4ddaa3d67c2aa8dc129e66db989be68fb621cdf
Flow9021 <floransiah@gmail.com>
2023-10-02 11:43:21 -0700

    libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -9,24 +9,21 @@
 
 int _atoi(char *s)
 {
-        int result = 0;
-        int sign = 1;
-        int i = 0;
+	int sign = 1;
+	int i = 0;
+	unsigned int res = 0;
 
-        while (i < strlen(s) && s[i] == ' ')
-        {
-                i++;
-        }
-        if (i < (int)strlen(s) && (s[i] == '+' || s[i] == '-'))
-        {
-                sign = 1; if (s[i] == '+)'
-                        else -1;
-                i++;
-        }
-        while (i < strlen(s) && s[i] >= '0' && s[i] <= '9')
-        {
-                result = result * 10 + (s[i] - '0');
-                i++;
-        }
-        return (result * sign);
+	while ((s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
 }
