e5c4ba36a2821c695f1235b7c17c0cb70b8bdaff
Flow9021 <floransiah@gmail.com>
2023-10-04 14:59:26 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the function
+ * @argc: command line argument
+ * @argv: command line argument
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	char *name = argv[0];
+
+	printf("%s\n", name);
+	return (0);
+}
