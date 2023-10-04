02012a587125280e528d7d0d5076b48c01a721b7
Flow9021 <floransiah@gmail.com>
2023-10-04 15:25:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the function
+ * @argc: command line argument
+ * @argv: command line argument
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
